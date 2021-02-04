1dc753f1429dcadd620829a112868174609253d4
SantiagoG <s.garavitor@uniandes.edu.co>
2021-02-03 20:48:14 -0500

    pr1

--- App/view.py
+++ App/view.py
@@ -42,7 +42,7 @@ def printMenu():
     print("Opciones:")
     print("1- Cargar Libros")
     print("2- Cargar Tags")
-    # TO-DO: Modificaciones para completar el laboratorio 1.
+    print("3- Cargar Book Tags")
     print("0- Salir")
 
 
@@ -58,6 +58,10 @@ def loadTags():
     Carga los Tags
     """
     return controller.loadTags('GoodReads/tags.csv')
+
+def loadBookTags():
+
+    return controller.loadBooksTags('GoodReads/book_tags-small.csv')
 
 
 """
@@ -75,7 +79,11 @@ while True:
         print("Cargando información de tags....")
         tags = loadTags()
         print('Total de tags cargados: ' + str(lt.size(tags)))
-    # TO-DO: Modificaciones para completar el laboratorio 1.
+
+    elif int(inputs[0]) == 3:
+        print("Cargando información de book tags....")
+        booktags = loadBookTags()
+        print('Total de book-tags cargados: ' + str(lt.size(booktags)))
 
     else:
         sys.exit(0)
